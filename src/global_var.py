e3ec5316695112cec1b43f9669d3b8c6f10aea92
N3mesis98 <adrienjacquet98@gmail.com>
2014-03-25 15:19:13 +0100

    changement du nom des polices

--- src/global_var.py
+++ src/global_var.py
@@ -5,7 +5,7 @@
 
 
 ## var ##
-police_list = ["centrale","coupable_maj_bas","coupable_maj_haut","coupable_min_bas","coupable_min_haut"]
+police_list = ["centrale","coupable_bas_maj","coupable_haut_maj","coupable_bas_min","coupable_haut_min"]
 js_db = "js_db.js"
 php_db = "php_db"
 
